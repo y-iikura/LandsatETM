73656bf28f2964dfa32026941e03fa3ad7a2f951
飯倉善和 <iikura@hirosaki-u.ac.jp>
2016-06-11 18:13:23 +0900

    Geometrical parameter (orbit and scan direction) and calibration coefficients is written in gparam.txt and all bands are processed and saved in specified forlder.

--- convert_util.py
+++ convert_util.py
@@ -5,6 +5,7 @@
 	Original data : Landsat 7 ETM+ : geotif format
 	New data : converted image with specified region 	
  12/27/2015 executable file
+ 6/11/2016 geometrical parameter calculation & output
 """
 import numpy as np
 import cv2
@@ -12,29 +13,137 @@ import cv2
 #from osgeo import osr
 import proj_util as pr
 
-
-#sun_el = 0.0
-#sun_az = 0.0
-
-#xs=0.0
-#ye=0.0
-#dx=0.0
-#dy=0.0
-
 class original:
   def __init__(self,fname):
+    self.name=fname
+    f=open(fname+'.met','r')
+    lines=f.readlines()
+    f.close()
+    lmax=[]
+    lmin=[]
+    for line in lines:
+      if line.find('SUN_AZIMUTH') != -1:
+        self.sun_az=float(line.split()[2])
+      if line.find('SUN_ELEVATION') != -1:
+        self.sun_el=float(line.split()[2])
+      if line.find('LMAX_BAND1') != -1:
+        lmax.append(float(line.split()[2]))
+      if line.find('LMIN_BAND1') != -1:
+        lmin.append(float(line.split()[2]))
+      if line.find('LMAX_BAND2') != -1:
+        lmax.append(float(line.split()[2]))
+      if line.find('LMIN_BAND2') != -1:
+        lmin.append(float(line.split()[2]))
+      if line.find('LMAX_BAND3') != -1:
+        lmax.append(float(line.split()[2]))
+      if line.find('LMIN_BAND3') != -1:
+        lmin.append(float(line.split()[2]))
+      if line.find('LMAX_BAND4') != -1:
+        lmax.append(float(line.split()[2]))
+      if line.find('LMIN_BAND4') != -1:
+        lmin.append(float(line.split()[2]))
+      if line.find('LMAX_BAND5') != -1:
+        lmax.append(float(line.split()[2]))
+      if line.find('LMIN_BAND5') != -1:
+        lmin.append(float(line.split()[2]))
+      if line.find('LMAX_BAND7') != -1:
+        lmax.append(float(line.split()[2]))
+      if line.find('LMIN_BAND7') != -1:
+        lmin.append(float(line.split()[2]))
+    self.offset=lmin[0:6]
+    self.gain=[(x-y)/255.0 for (x,y) in zip(lmax[0:6],lmin[0:6])]
+    fname=self.name+'_z54_nn10.tif'
     gt,wkt,image=pr.read_tif(fname)
     self.image=image
+    self.jmax,self.imax=image.shape
     self.xs=gt[0]
     self.ye=gt[3]
     self.dx=gt[1]
     self.dy=-gt[5]
+    self.pc=self.imax/2.0
+    self.lc=self.jmax/2.0
+    self.xc=self.xs+self.dx*self.pc
+    self.yc=self.ye-self.dy*self.lc
+    self.t0=0.0
+    self.angle=0.0
+    self.hsat=691650.00
+  def read_band(self,band):
+    fname=self.name+'_z54_nn'+str(band)+'.tif'
+    gt,wkt,image=pr.read_tif(fname)
+    self.image=image
   def display(self,name,xmax,ymax):
     old=self.image
     min=np.min(old) ; max=np.max(old)
     oldx=np.uint8(255.0*(old-min)/(max-min))
     oldy=cv2.resize(oldx,(xmax,ymax))
     cv2.imshow(name,oldy)
+  def xy2pl(self,x,y):
+    aa=np.cos(self.t0)/self.dx
+    bb=-np.sin(self.t0)/self.dy
+    p=aa*(x-self.xs)+bb*(y-self.ye)
+    l=bb*(x-self.xs)-aa*(y-self.ye)
+    return [p,l]
+  def pl2xy(self,p,l):
+    aa=np.cos(self.t0)*self.dx
+    bb=-np.sin(self.t0)*self.dy
+    x=aa*p+bb*l+self.xs
+    y=bb*p-aa*l+self.ye
+    return [x,y]
+  def coverage(self):
+    ul=self.pl2xy(0.0,0.0) 
+    ur=self.pl2xy(float(self.imax),0.0)
+    ll=self.pl2xy(0.0,float(self.jmax))
+    lr=self.pl2xy(float(self.imax),float(self.jmax)) 
+    left=int(ll[0]/10000.0)
+    right=int(np.ceil(ur[0]/10000.0)) 
+    lower=int(lr[1]/10000.0) 
+    upper=int(np.ceil(ul[1]/10000.0))
+    return [left,right,lower,upper]
+  def chokka(self):
+    mask=np.zeros((self.jmax,self.imax),dtype=np.uint8)
+    temp=np.where(self.image > 0) 
+    mask[temp]=1
+    xtemp = temp[1]
+    ytemp = temp[0]
+    set1=np.min(ytemp)
+    set2=np.max(xtemp)
+    set3=np.max(ytemp)
+    set4=np.min(xtemp)
+    state=0
+    position=[0]
+    for i in range(self.imax):
+      if mask[set1,i] != state:
+        position.append(i)
+        state=1-state
+    state=0
+    for j in range(self.jmax):
+      if mask[j,set2] != state:
+        position.append(j)
+        state=1-state
+    state=0
+    for i in range(self.imax):
+      if mask[set3,i] != state:
+        position.append(i)
+        state=1-state
+    state=0
+    for j in range(self.jmax):
+      if mask[j,set4] != state:
+        position.append(j)
+        state=1-state
+    tq1=float(position[1]-set4)/(position[7]-set1)
+    tp1=float(position[3]-set1)/(set2-position[2])
+    tq2=float(set2-position[6])/(set3-position[4])
+    tp2=float(set3-position[8])/(position[5]-set4)
+    tp=np.arctan((tp1+tp2)/2.0)
+    tq=np.arctan((tq1+tq2)/2.0)
+    self.pv=[np.cos(tp),-np.sin(tp)]
+    self.qv=[-np.sin(tq),-np.cos(tq)]
+    #pq=transpose([[pv],[qv]])
+    #pinv=invert(pq)
+    #angle=angle*!dtor
+    #angle2=asin((earth+hsat)*sin(angle)/earth)
+    #dn=earth*(angle2-angle)
+
 
 class convert:
   def __init__(self,old,xmax,ymax):
@@ -61,4 +170,32 @@ class convert:
     res=np.corrcoef(inc[100:500,100:500].flat,new[100:500,100:500].flat)
     return -res[0,1]
 
-
+def gwrite(sat,fnew):
+  f=open('../'+fnew+'/gparm.txt','w')
+  f.write(" * datum : WGS84\n")
+  f.write(" image size:\n")  f.write("  pixel= "+str(sat.imax)+"\n")  f.write("  line = "+str(sat.jmax)+"\n")  f.write("image scene center:\n")  f.write("  p0= "+str(sat.pc)+"\n")  f.write("  l0= "+str(sat.lc)+"\n")  f.write("utm scene center:\n")  f.write("  x0= "+str(sat.xc)+"\n")  f.write("  y0= "+str(sat.yc)+"\n")  f.write("spatial resolution:\n")  f.write("  dx0= "+str(sat.dx)+"\n")  f.write("  dy0= "+str(sat.dy)+"\n")  f.write("orientation angle:\n")  f.write("  t0= "+str(sat.t0)+"\n")
+  f.write("pointing angle:\n")
+  f.write("  angle= "+str(sat.angle)+"\n")  f.write("sun position:\n")  f.write("  el= "+str(sat.sun_el)+"\n")  f.write("  az= "+str(sat.sun_az)+"\n")
+  pvx=['{:10.6f}'.format(x) for x in sat.pv]
+  qvx=['{:10.6f}'.format(x) for x in sat.qv]
+  f.write("scanning direction:\n")
+  f.write("  pv= "+" ".join(pvx)+"\n")
+  f.write("orbit direction:\n")
+  f.write("  qv= "+" ".join(qvx)+"\n")
+  f.write("nadir utm point:\n")
+  f.write("  xn=  "+str(sat.xc)+"\n")
+  f.write("  yn=  "+str(sat.yc)+"\n")
+  f.write("satellite height:"+"\n")
+  f.write("  hsat= "+str(sat.hsat)+"\n")
+  left,right,lower,upper=sat.coverage()
+  f.write("coverage:"+"\n")
+  f.write("  left = "+str(left)+"\n")
+  f.write("  right= "+str(right)+"\n")
+  f.write("  lower= "+str(lower)+"\n")
+  f.write("  upper= "+str(upper)+"\n")
+  offsetx=['{:7.2f}'.format(x) for x in sat.offset]
+  gainx=['{:7.3f}'.format(x) for x in sat.gain]
+  f.write("calibration:"+"\n")
+  f.write("  offset= "+" ".join(offsetx)+"\n")
+  f.write("  gain=   "+" ".join(gainx)+"\n")
+  f.close()
--- extraction.py
+++ extraction.py
@@ -5,7 +5,8 @@
 	Original data : Landsat 7 ETM+ : geotif format
 		Specified Region from DEM Geotif Image region 	
 """
-# extraction.py ELP108R032_7T20020630 template.txt
+# cd /Volumes/Transcend/LandsatETM+
+# extraction.py ELP108R032_7T20020630 template.txt NEW
 
 import sys
 import os
@@ -16,16 +17,17 @@ import proj_util as pr
 
 param=sys.argv
 if len(param)!=3:
-    print 'Usage: extraction.py scene_name area_file'
-
+    print 'Usage: extraction.py scene_name area_file new_folder'
 
 fscene=param[1]
-
 fname=param[2]
+fnew=param[3]
+
+if os.path.isdir(fnew) == False: os.mkdir(fnew)  
+
 f=open(fname)
 lines=f.readlines()
 f.close()
-
 for line in lines:
   if line.find('xs')!=-1: pr.xs=float(line.split()[1])
   if line.find('xe')!=-1: pr.xe=float(line.split()[1])
@@ -35,13 +37,6 @@ for line in lines:
   if line.find('dy')!=-1: pr.dy=float(line.split()[1])
   print line,
 
-#pr.xs= 406200.0
-#pr.xe= 442200.0
-#pr.dx= 30.0
-#pr.ys= 4460750.0
-#pr.ye= 4496750.0
-#pr.dy= 30.0
-
 xmax=int((pr.xe-pr.xs)/pr.dx)
 ymax=int((pr.ye-pr.ys)/pr.dy)
 print xmax,ymax
@@ -50,22 +45,29 @@ print xmax,ymax
 os.chdir(fscene)
 
 list=os.listdir('.')
-
 for name in list:
   if name.find('.met')!=-1:
     fname=name[:-4]
 
-#fname='p108r032_7t20020630'
+flag=1
+sat=ut.original(fname)
+if flag:
+  print sat.jmax,sat.imax
+  print sat.xs,sat.dx
+  print sat.ye,sat.dy
+  print sat.offset
+  print sat.gain
+
+sat.chokka()
+if flag:
+  print sat.pv
+  print sat.qv
 
 for band in [10,20,30,40,50,70]:
-  sat=ut.original(fname+'_z54_nn'+str(band)+'.tif')
-  sat.image.shape
-  #sat.display('sat',600,600)
-  #cv2.destroyWindow('sat')
+  sat.read_band(band)
   conv=ut.convert(sat,xmax,ymax)
   new=conv.convert(0.0,0.0)
-  #cv2.imshow('new',cv2.resize(new,(600,600)))
-  #cv2.destroyWindow('new')
-  pr.write_tif('../band'+str(band/10)+'.tif',new,1)
+  pr.write_tif('../'+fnew+'/band'+str(band/10)+'.tif',new,1)
 
+ut.gwrite(sat,fnew)
 exit()
